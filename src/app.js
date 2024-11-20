60707d34b0a89785b8489db5f9a5d72e0ab9a393
Luis Carlos Jesus Pacheco <lcjpacheco@outlook.com>
2024-11-20 12:25:01 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quién = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let acción = [" comió", " pisó", " aplastó", " rompió"];
+  let qué = [" mi tarea", " mi teléfono", " el auto"];
+  let cuando = [
+    " antes de la clase",
+    " cuando dormía",
+    " mientras hacía ejercicio",
+    " durante mi almuerzo",
+    " mientras oraba"
+  ];
+
+  let rdm1 = Math.floor(Math.random() * quién.length);
+  let rdm2 = Math.floor(Math.random() * acción.length);
+  let rdm3 = Math.floor(Math.random() * qué.length);
+  let rdm4 = Math.floor(Math.random() * cuando.length);
+
+  document.querySelector("#excusa").innerHTML =
+    quién[rdm1] + acción[rdm2] + qué[rdm3] + cuando[rdm4];
 };
